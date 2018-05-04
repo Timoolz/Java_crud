d4dd06da32665189e9e64178bf11fb87f2b04942
Laleye Olamide <laleyeolamide@yahoo.com>
2018-05-04 17:45:20 +0100

    debug

--- src/main/java/com/sdua/api/config/AppConfiguration.java
+++ src/main/java/com/sdua/api/config/AppConfiguration.java
@@ -3,10 +3,21 @@ package com.sdua.api.config;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class AppConfiguration {
-    @JsonProperty("appBaseUrl")
-    private String appBaseUrl;
+//    @JsonProperty("appBaseUrl")
+//    private String appBaseUrl;
+//
+//    public String getAppBaseUrl() {
+//        return appBaseUrl;
+//    }
 
-    public String getAppBaseUrl() {
-        return appBaseUrl;
+    @JsonProperty("allowedOrigin")
+    private String allowedOrigin;
+
+    public String getAllowedOrigin() {
+        return allowedOrigin;
     }
+
+
+
+
 }
